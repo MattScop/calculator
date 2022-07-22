50c78af356b99a2182d2b7b2293c98e4554be6de
MattScop <matteo.scopece@outlook.com>
2022-07-22 09:47:54 +0200

    Add Second Step to JS --READ COMMENT BELOW--
    Added feature which allows the numbers to go to secondary line
    when operator is present on input, for improved usability and
    readability.

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
 /* 
-FIRST STEP:
+FIRST STEP: --DONE--
 When Click Number, It Appears On The Screen;
 */
 
+/* 
+SECOND STEP:
+The number inserted AFTER the operator must start with a fresh line,
+placing the old number on the secondary line
+*/
+
 const buttonsNumber = document.getElementsByClassName('number');
+const buttonsCalc = document.getElementsByClassName('calc');
+const multiplier = document.querySelector('.multiplier');
+const divider = document.querySelector('.divider');
+const firstLineNumbers = document.querySelector('.first-line-numbers');
+const firstLineCalcs = document.querySelector('.first-line-calcs');
+const secondaryLineNumbers = document.querySelector('.secondary-line-numbers');
+const secondaryLineCalcs = document.querySelector('.secondary-line-calcs');
 
 // Create array for numbers inserted
 const arrButtonsNumber = [];
-
-const buttonsCalc = document.getElementsByClassName('calc');
-const multiplier = document.querySelector('.multiplier');
-const divider = document.querySelector('.divider')
-const displayNumber = document.querySelector('.display-numbers');
-const displayCalc = document.querySelector('.display-calcs');
 
 // Listen to mouse click on each button
 Array.from(buttonsNumber).forEach(button => {
     button.addEventListener('click', (e) => {
 
         // Allow 10 numbers input max
-        if (displayNumber.innerText.length <= 9) {
-            displayNumber.innerText += parseInt(e.target.innerText);
-            
-            // Place number in array to allow for DEL button to work later
-            arrButtonsNumber.push(parseInt(e.target.innerText));
+        if (firstLineNumbers.innerText.length <= 9) {
+
+            // Go to secondary line if operator is present on number input
+            if (firstLineCalcs.innerText !== "" ) {
+                secondaryLineNumbers.innerText = firstLineNumbers.innerText;
+                secondaryLineCalcs.innerText = firstLineCalcs.innerText;
+                firstLineNumbers.innerText = "";
+                firstLineCalcs.innerText = "";
+                firstLineNumbers.innerText += parseInt(e.target.innerText);
+            } else {
+                firstLineNumbers.innerText += parseInt(e.target.innerText);
+
+                // Place number in array to allow for DEL button to work later
+                arrButtonsNumber.push(parseInt(e.target.innerText));
+            }
+
         } else {
             return
         }
     })
 })
 
-// Add operators always at the end
+// Add operators at the end
 Array.from(buttonsCalc).forEach(button => {
     button.addEventListener('click', (e) => {
         
         // change multiplier and divider style to better fit the font
         if (e.target.classList.contains('multiplier')) {
-            displayCalc.innerText = '*';
+            firstLineCalcs.innerText = '*';
         } else if (e.target.classList.contains('divider')){
-            displayCalc.innerText = "/";
+            firstLineCalcs.innerText = "/";
         } else {
-            displayCalc.innerText = e.target.innerText
+            firstLineCalcs.innerText = e.target.innerText
         }
     })
 })
